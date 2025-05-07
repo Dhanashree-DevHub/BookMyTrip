c78dff721bbe0e05c8322d0a222d0258ed51e07c
Dhanashree-DevHub <ranadedhanu16@gmail.com>
2025-05-08 00:37:17 +0530

    Implemented crud apis for booking model

--- bus_booking/urls.py
+++ bus_booking/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Travels.urls')),
-
+    path('api/', include('Travels.api_urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
